85df39d8d2d285bf6e77c956f4f44e09447a4e1a
trivedidn <svw6dn@virginia.edu>
2024-06-14 16:39:19 -0400

    new histogram

--- Histogram Project.py
+++ Histogram Project.py
@@ -7,7 +7,14 @@ print("hello")
 
 data = data[['Hard Drive Size (in GB)', 'RAM (in GB)']]
 
-data.plot()
+plt.hist(data, bins=30, edgecolor='black')  # Plot histogram with 30 bins
+plt.title('Histogram of Data')
+plt.xlabel('Hard Drive Size')
+plt.ylabel('RAM')
+plt.grid(True)  # Add gridlines to the plot
 plt.show()
+
+
+#we were having dificulty with opening the file - only had time for a bar chart
 
 
